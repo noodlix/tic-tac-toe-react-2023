43c290eb2f4e7a72e654e27caee956a1f288d250
zhibek ʚ♡ɞ <111293401+noodlix@users.noreply.github.com>
2023-06-07 00:05:48 +0600

    Update Grid.jsx

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,17 +1,24 @@
 export default function Grid(props) {
-  return (
-    <div className="grid grid-cols-3 m-auto w-fit">
-      {props.grid.map((element) => (
-        <div key={element.id} className="relative group">
-          <div className="absolute transition duration-500 opacity-75 -inset-1 bg-gradient-to-r from-slate-950 via-red-600 to-stone-700 blur group-hover:opacity-100"></div>
-          <button
-            onClick={() => props.handlePlay(element.id)}
-            className="relative w-24 h-24 transition duration-500 bg-slate-950 hover:bg-slate-800"
-          >
-            {element.text}
-          </button>
-        </div>
-      ))}
-    </div>
-  );
-}
+    let squareclass =  "relative w-24 h-24 transition duration-100 bg-slate-500 hover:bg-slate-800 text-white " 
+    let wincomboclass =  "relative w-24 h-24 transition duration-100 bg-green-300 hover:bg-green-600 text-white " 
+
+
+
+    return (
+      <div className="grid grid-cols-3 m-auto w-fit">
+        {props.grid.map((element) => (
+          <div key={element.id} className="relative group">
+            <div className="absolute transition duration-500 opacity-75 -inset-1 group-hover:opacity-100"></div>
+
+
+            <button
+              onClick={() => props.handlePlay(element.id)}
+              className={(element.id == props.wincombo)? wincomboclass : squareclass}
+            >
+              {element.text}
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+  }
